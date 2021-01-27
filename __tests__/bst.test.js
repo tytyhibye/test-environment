e93547038411d596e4988b21e71c70d547cfd9d7
Tyler Bates <tylerbatesdevdesign@gmail.com>
2021-01-27 09:12:30 -0800

    implement logic for testing functioning binary search tree.

---
+++ __tests__/bst.test.js
@@ -0,0 +1,45 @@
+import BST from '../src/bst.js';
+import BSTNode from '../src/bst-node.js';
+
+describe('binarySearchTree', () => {
+    test('should initialize a binary search tree with a root of null', () => {
+        let bst = new BST();
+        expect(bst.root).toEqual(null);
+    });
+
+    test('it should create a new root node', () => {
+        let bst = new BST();
+        let node = new BSTNode(36);
+        bst.insertNode(node);
+        expect(bst.root).toEqual(node);
+      });
+
+      test('it should add a child node to the left of the root node', () => {
+        let bst = new BST();
+        let rootNode = new BSTNode(36);
+        bst.insert(rootNode);
+        let newNode = new BSTNode(22);
+        bst.insert(newNode);
+        expect(rootNode.left.data).toEqual(22);
+      });
+
+      test('it should add a child node to the right of the root node', () => {
+          let bst = new BST();
+          let rootNode = new BSTNode(36);
+          bst.insert(rootNode);
+          let newNode = new BSTNode(48);
+          bst.insert(newNode);
+          expect(rootNode.right.data).toEqual(48);
+      });
+
+      test('it should add a child to the left of a child node', () => {
+          let bst = new BST();
+          let rootNode = new BSTNode(36);
+          bst.insert(rootNode);
+          let node2 = new BSTNode(22);
+          bst.insert(node2);
+          let node3 = new BSTNode(11);
+          bst.insert(node3);
+          expect(rootNode.left.left.data).toEqual(11);
+      });
+});
---
+++ src/bst.js
@@ -0,0 +1,13 @@
+insert(insertedNode) {
+    if (this.root === null) {
+      this.root = insertedNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (currentNode.data > insertedNode.data) {
+          if ()
+        } 
+      }
+    }
+  }
+}
